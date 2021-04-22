7d029c11b0510b797ee8a569d1617a8e5015c255
Hendrik de Bruin <hendrik.s.debruin@gmail.com>
2021-04-22 16:59:40 +0200

    feat: add hook to detect .pacsave, .pacnew

---
+++ etc/pacman.d/hooks/locate_pacnew_pacsave.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+pacman_files=( $(locate pacnew pacsave))
+len=${#pacman_files[@]}
+
+RED='\033[0;31m'
+NORNAL_COLOR='\033[0m'
+
+if [[ $len -gt 0 ]];
+then
+	echo -e "${RED}WARNING: pacnew/pacsave files detected on system:${NORNAL_COLOR}"
+	for file in "${pacman_files[@]}"
+	do
+		echo "    $file"
+	done
+fi
+
+
